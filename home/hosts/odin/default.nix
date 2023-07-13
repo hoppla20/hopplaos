c7626b0a8bba8e3469d5abad6448fbd159cd6db0
vincent.cui <privat@vincentcui.de>
2023-07-14 00:36:18 +0200

    add odin home profile

---
+++ home/hosts/odin/default.nix
@@ -0,0 +1,17 @@
+{
+  hopplaos.hardware = {
+    monitors = [
+      {
+        name = "eDP-1";
+        value = {
+          resolution = "1920x1080";
+          refreshRate = 60;
+          position = {
+            x = 0;
+            y = 0;
+          };
+        };
+      }
+    ];
+  };
+}
