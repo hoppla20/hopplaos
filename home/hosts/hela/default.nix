261174cfd2182262f49a5b645a82d2c95623d49c
vincent.cui <privat@vincentcui.de>
2023-07-13 16:14:03 +0200

    fix hela monitor setup

--- home/hosts/hela/default.nix
+++ home/hosts/hela/default.nix
@@ -2,18 +2,18 @@
   hopplaos.hardware = {
     monitors = [
       {
-        name = "DP-1";
+        name = "DP-2";
         value = {
           resolution = "1920x1080";
           refreshRate = 60;
           position = {
-            x = 1080;
+            x = 1920;
             y = 0;
           };
         };
       }
       {
-        name = "DP-2";
+        name = "DP-3";
         value = {
           resolution = "1920x1080";
           refreshRate = 60;
