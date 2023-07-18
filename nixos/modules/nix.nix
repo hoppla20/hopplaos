f55ebeabd8afc9c7c6eb61dc1f3f8bf286752a05
vincent.cui <privat@vincentcui.de>
2023-07-18 19:45:23 +0200

    update nix configuration

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -17,11 +17,13 @@
     gc.automatic = true;
     optimise.automatic = true;
     extraOptions = ''
-      min-free = 536870912
+      experimental-features = nix-command flakes
+
       keep-outputs = true
       keep-derivations = true
+
+      min-free = 536870912
       fallback = true
-      experimental-features = nix-command flakes
     '';
   };
 
