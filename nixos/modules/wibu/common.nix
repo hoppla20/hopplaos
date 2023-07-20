7906add828bf3423657fae0cce750c3083b60678
vincent.cui <privat@vincentcui.de>
2023-07-20 11:44:41 +0200

    nixos(wibu): let nix fetch certificates

--- nixos/modules/wibu/common.nix
+++ nixos/modules/wibu/common.nix
@@ -13,6 +13,11 @@
     mkMerge
     ;
 
+  inherit
+    (pkgs)
+    fetchurl
+    ;
+
   cfg = config.hopplaos.wibu;
 in {
   options.hopplaos.wibu = {
@@ -20,7 +25,16 @@ in {
   };
 
   config = mkIf cfg.enable {
-    security.pki.certificateFiles = lib.filesystem.listFilesRecursive ./certificates;
+    security.pki.certificateFiles = [
+      (fetchurl {
+        url = "https://intranet.wibu.local/WIBU_Root_CA_II.crt";
+        sha256 = "0vql98m89jhxm1lgm0hx2zw8x0dqnbi69ynv17hlrw99d9i4gdr5";
+      })
+      (fetchurl {
+        url = "https://intranet.wibu.local/WIBU_HTTPS_CA_II.crt";
+        sha256 = "16lgib95a7qddanmc6fvf5pxd96a77bfcy67ikfd8271xmjic3hr";
+      })
+    ];
 
     environment.systemPackages = [pkgs.vmware-workstation];
   };
