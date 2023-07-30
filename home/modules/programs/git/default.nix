69e5be5d9e0ac8ecfa94892a8eee4cad0e98a8c3
vincent.cui <privat@vincentcui.de>
2023-07-30 12:29:14 +0200

    home: add program.git module

---
+++ home/modules/programs/git/default.nix
@@ -0,0 +1,43 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  inherit
+    (lib)
+    types
+    mkOption
+    mkEnableOption
+    mkIf
+    mkMerge
+    ;
+
+  cfg = config.hopplaos.programs.git;
+in {
+  options.hopplaos.programs.git = {
+    enable = mkEnableOption "Git";
+
+    userName = mkOption {
+      type = types.str;
+    };
+    userEmail = mkOption {
+      type = types.str;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      diff-so-fancy.enable = true;
+
+      extraConfig = {
+        core.editor = "nvim";
+        pull.rebase = "false";
+        init.defaultBranch = "main";
+      };
+
+      inherit (cfg) userName userEmail;
+    };
+  };
+}
