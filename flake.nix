535c12a8c186313993437c912d48659fd068b5f9
vincent.cui <privat@vincentcui.de>
2023-08-03 20:11:36 +0200

    update nix substituters

--- flake.nix
+++ flake.nix
@@ -167,7 +167,11 @@
 
   nixConfig = {
     extra-experimental-features = "nix-command flakes";
-    extra-substituters = ["https://nix-community.cachix.org" "https://hyprland.cachix.org"];
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+    ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -13,6 +13,17 @@
       auto-optimise-store = true;
       allowed-users = ["@wheel"];
       trusted-users = ["@wheel"];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nrdxp.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="
+      ];
     };
     gc.automatic = true;
     optimise.automatic = true;
