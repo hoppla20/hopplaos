d87fde9fe2bda007d7aa79ba2687e5ca04c234d7
vincent.cui <privat@vincentcui.de>
2023-09-09 15:01:43 +0200

    fix cross-arch module

--- nixos/modules/cross-arch/default.nix
+++ nixos/modules/cross-arch/default.nix
@@ -5,7 +5,7 @@
 }: let
   cfg = config.hopplaos.cross-arch;
 in {
-  options.hoppla.cross-arch.enable = lib.mkEnableOption "Cross/Qemu native compilation";
+  options.hopplaos.cross-arch.enable = lib.mkEnableOption "Cross/Qemu native compilation";
 
   config = lib.mkIf cfg.enable {
     boot.binfmt.emulatedSystems = ["aarch64-linux"];
