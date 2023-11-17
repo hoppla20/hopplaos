5b397ebd92b9b8a21bbaba68fd8decc10e7ae207
vincent.cui <privat@vincentcui.de>
2023-11-17 17:38:43 +0100

    home: add desktop.development module

---
+++ home/modules/desktop/development/default.nix
@@ -0,0 +1,25 @@
+{
+  pkgs-unstable,
+  config,
+  lib,
+  ...
+}: let
+  desktopCfg = config.hopplaos.desktop;
+  cfg = desktopCfg.development;
+in {
+  options = {
+    hopplaos.desktop.development = {
+      enable = lib.mkEnableOption "Development" // {default = desktopCfg.enable;};
+    };
+  };
+
+  config = lib.mkIf (desktopCfg.enable && cfg.enable) {
+    home.packages = builtins.attrValues {
+      inherit
+        (pkgs-unstable)
+        powershell
+        yaml-language-server
+        ;
+    };
+  };
+}
