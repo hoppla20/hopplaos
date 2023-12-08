bb7036cc7f3ddd4fe28d7fc065dd234b6c29e856
vincent.cui <privat@vincentcui.de>
2023-12-08 12:58:51 +0100

    nixos(wibu): add certificates to repo instead of download

--- nixos/modules/wibu/common.nix
+++ nixos/modules/wibu/common.nix
@@ -13,20 +13,8 @@ in {
   options.hopplaos.wibu = {enable = mkEnableOption "WIBU";};
 
   config = mkIf cfg.enable {
-    security.pki.certificateFiles = [
-      ./certificates/gsrsaovsslca2018.crt
-      (fetchurl {
-        url = "https://intranet.wibu.local/WIBU_Root_CA_II.crt";
-        sha256 = "0vql98m89jhxm1lgm0hx2zw8x0dqnbi69ynv17hlrw99d9i4gdr5";
-      })
-      (fetchurl {
-        url = "https://intranet.wibu.local/WIBU_HTTPS_CA_II.crt";
-        sha256 = "16lgib95a7qddanmc6fvf5pxd96a77bfcy67ikfd8271xmjic3hr";
-      })
-    ];
-
+    security.pki.certificateFiles = lib.filesystem.listFilesRecursive ./certificates;
     services.teamviewer.enable = true;
-
     environment.systemPackages = builtins.attrValues {
       inherit
         (pkgs)
