cbc705e150ba82c2b7b63c812fbd7ebb1bb381dc
vincent.cui <privat@vincentcui.de>
2024-02-17 14:02:25 +0100

    nixos: rework power-management module

--- nixos/hosts/odin/default.nix
+++ nixos/hosts/odin/default.nix
@@ -10,7 +10,6 @@
       cpu.manufacturer = "amd";
       gpu.manufacturer = "amd";
     };
-    services.power-management.hasBattery = true;
   };
 
   boot.initrd.availableKernelModules = ["nvme" "ehci_pci" "xhci_pci" "rtsx_pci_sdmmc"];
