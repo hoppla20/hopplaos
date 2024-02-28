605c171a58d31c2d333e24630031cc055ed35652
vincent.cui <privat@vincentcui.de>
2024-02-28 18:03:29 +0100

    home/desktop: fix gtk4 theme

--- home/modules/desktop/common.nix
+++ home/modules/desktop/common.nix
@@ -184,6 +184,9 @@ in {
       mime.enable = true;
       configFile = {
         "wallpapers".source = ./wallpapers;
+        "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+        "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+        "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
       };
     };
   };
