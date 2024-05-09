e2d50d97a0f3eb9d0c6160452d67590a77a19ca2
vincent.cui <privat@vincentcui.de>
2024-05-09 21:56:01 +0200

    gnome update

--- home/modules/desktop/gnome.nix
+++ home/modules/desktop/gnome.nix
@@ -82,11 +82,18 @@ in {
         switch-group-backward = [];
         switch-panels = [];
         switch-panels-backward = [];
-        switch-to-workspace-1 = [];
         switch-to-workspace-last = [];
         switch-to-workspace-left = [];
         switch-to-workspace-right = [];
         unmaximize = [];
+        move-to-workspace-1 = ["<Shift><Super>1"];
+        move-to-workspace-2 = ["<Shift><Super>2"];
+        move-to-workspace-3 = ["<Shift><Super>3"];
+        move-to-workspace-4 = ["<Shift><Super>4"];
+        switch-to-workspace-1 = ["<Super>1"];
+        switch-to-workspace-2 = ["<Super>2"];
+        switch-to-workspace-3 = ["<Super>3"];
+        switch-to-workspace-4 = ["<Super>4"];
       };
       "org/gnome/shell/extensions/paperwm/keybindings" = {
         close-window = ["<Super>q"];
